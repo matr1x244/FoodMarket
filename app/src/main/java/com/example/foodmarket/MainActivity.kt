bb34e68abad5fe6004b057d2362e47ba5b5effdb
matr1x244 <zio-21@yandex.ru>
2023-06-08 19:19:14 +0300

    add bottomNav in Main and FoodsFragment

--- app/src/main/java/com/example/foodmarket/MainActivity.kt
+++ app/src/main/java/com/example/foodmarket/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.foodmarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.foodmarket.ui.main.FoodsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.main_fragment_container, FoodsFragment.newInstance())
+                .commitNow()
+        }
+
+        bottomNavMenu()
+    }
+
+    private fun bottomNavMenu() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.foods_menu -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.main_fragment_container, FoodsFragment.newInstance())
+                        .addToBackStack(null)
+                        .commit()
+                }
+                R.id.search_menu -> {
+                    Toast.makeText(this, "search_menu", Toast.LENGTH_SHORT).show()
+                }
+                R.id.basket_menu -> {
+                    Toast.makeText(this, "basket_menu", Toast.LENGTH_SHORT).show()
+                }
+                R.id.profile_menu -> {
+                    Toast.makeText(this, "profile_menu", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
+        bottomNavigationView.selectedItemId = R.id.foods_menu
     }
 }
---
+++ app/src/main/java/com/example/foodmarket/ui/main/FoodsFragment.kt
@@ -0,0 +1,36 @@
+package com.example.foodmarket.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.foodmarket.databinding.FragmentFoodsBinding
+
+class FoodsFragment: Fragment() {
+
+    companion object {
+        fun newInstance() = FoodsFragment()
+    }
+
+    private var _binding: FragmentFoodsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFoodsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
